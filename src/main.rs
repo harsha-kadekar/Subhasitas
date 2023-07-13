c7989a5502be4a901c43392f0a10c0bf6e37d11d
Harsha Kadekar <harsha.kadekar@gmail.com>
2023-07-12 22:51:57 -0700

    initial hello world - still errors

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use std::convert::Infalliable;
+use std::net::SocketAddr;
+
+use http_body_util::Full;
+use hyper::body::Bytes;
+use hyper::server::conn::http1;
+use hyper::service::service_fn;
+use hyper::{Request, Response};
+use tokio::net::TcpListener;
+
+
+async fn hello(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infalliable> {
+    Ok(Response::new(Full::new(Byptes::from("Namaste!"))))
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    let listener = TcpListener::bind(addr).await?;
+
+    loop {
+        let (stream, _) = listener.accept().await?;
+
+        tokio::task::spawn(async move {
+            if let Err(err) = http1::Builder::new()
+                .serve_connection(stream, service_fn(hello))
+                    .await
+                    {
+                        println!("Error serving connection: {:?}", err);
+                    }
+        });
+    }
 }
